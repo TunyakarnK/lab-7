790b53abdd2319b9290900b83e3a97e7fbce95b5
TunyakarnK <tunyakarn2544@gmail.com>
2021-01-04 17:54:34 +0700

    done

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+return (x>90 ? 'A' : x>75 ? 'B' : x>60 ? 'C' : x>45 ? 'D' : 'F');
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +20,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,27 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int age,h,p;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h<160){
+        	cout << "Your status = UNFRIEND";
+		}else if (h<175){
+			cout << "Your status = FRIEND";
+		}else {
+        cout <<"Enter your property: ";
+        cin >> p;
+        	cout << "Your status = " << (p>69e6 ? "MARRIED" :"ONE-NIGHT-STAND");
+		}
+    }else if(age <30){
+    	cout <<"Enter your property: ";
+        cin >> p;
+        cout << "Your status = " << (p>10e6 ? "BEST FRIEND" :"UNFRIEND");
+	}else{
+		cout << "Your status = UNFRIEND";
+	}
+    }
